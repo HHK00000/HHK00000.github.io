b322f86fadfb59a9d0582ebb934f890946e38ba6
hanhuankang <790182710@qq.com>
2026-03-18 11:04:01 +0800

    完善sh脚本

--- copy.sh
+++ copy.sh
@@ -1,4 +1,3 @@
-
 # 定义源目录和目标目录（固定配置，无需修改）
 SOURCE1="./HHK00000.github.io/index.html"
 SOURCE2="./HHK00000.github.io/assets/"
@@ -7,9 +6,12 @@ DEST1="./"
 DEST2="./assets/"
 DEST3="./document/"
 
-cp -rvpf "./HHK00000.github.io/index.html" "$DEST1"
-cp -rvpf "./HHK00000.github.io/assets/" "assets"
-cp -rvpf "./HHK00000.github.io/document/" "document"
+cp -rvpf "./HHK00000.github.io/index.html" "./"
+cp -rvpf "./HHK00000.github.io/404.html" "./"
+cp -rvpf "./HHK00000.github.io/hashmap.json" "./"
+cp -rvpf "./HHK00000.github.io/vp-icons.css" "./"
+cp -rvpf "./HHK00000.github.io/assets/" "./assets"
+cp -rvpf "./HHK00000.github.io/document/" "./document"
 
 
 # 脚本正常结束
